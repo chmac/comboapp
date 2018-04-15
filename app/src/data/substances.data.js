7bbaa4fad3b823a58c2cd2baa7efa78a106f8fa6
Callum Macdonald <git@callum-macdonald.com>
2018-04-15 17:30:57 +0200

    Build substance list from config.

--- app/src/data/substances.data.js
+++ app/src/data/substances.data.js
@@ -1,13 +1,58 @@
 // @flow
 
 import keyBy from "lodash/fp/keyBy";
+import flatten from "lodash/fp/flatten";
+import zip from "lodash/fp/zip";
+import map from "lodash/fp/map";
+import each from "lodash/fp/each";
+
+import config from "./config.json";
 
 export type Substance = {
   id: string,
   name: string
 };
 
-const substances = [
+// Build a list of "proper" names from `config.tableOrder`
+const names = flatten(config.tableOrder);
+
+// Build a list of groups along with their substances
+const groups = zip(config.groupNames, names);
+/*
+const groups = [
+  ['psychedelic', [
+      "LSD",
+      "Mushrooms",
+      "DMT",
+      "Mescaline",
+      "DOx",
+      "NBOMes",
+      "2C-x",
+      "2C-T-x",
+      "5-MeO-xxT",
+      "Cannabis"
+    ]
+  ],
+  ...
+]
+*/
+
+const nameToType = {};
+const groupPairs = map(([type, names]) => {
+  each(name => {
+    nameToType[name] = type;
+  })(names);
+})(groups);
+
+const substances = map(name => {
+  return {
+    id: name.toLowerCase(),
+    name,
+    type: nameToType[name]
+  };
+})(names);
+
+const substancesTemp = [
   { id: "lsd", name: "LSD" },
   {
     id: "cannabis",
