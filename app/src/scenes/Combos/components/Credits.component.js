46fa5e786b371503d9efb6157d8ebbbd2d367c45
Callum Macdonald <git@callum-macdonald.com>
2018-04-15 18:33:59 +0200

    Add spacing below credits.

--- app/src/scenes/Combos/components/Credits.component.js
+++ app/src/scenes/Combos/components/Credits.component.js
@@ -4,7 +4,7 @@ import React from "react";
 
 const Credits = () => {
   return (
-    <div style={{ marginTop: 50 }}>
+    <div style={{ marginTop: 50, marginBottom: 20 }}>
       Data sourced from the aweseome folks at{" "}
       <a href="http://wiki.tripsit.me/wiki/Drug_combinations" target="_blank">
         TripSit
