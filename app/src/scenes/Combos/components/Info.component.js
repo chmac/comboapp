e7f7c94842cf0dc4f3fc7b5fc6f1f9d3e331f76d
Bjørn <bjorn@tripsit.me>
2018-09-07 17:53:56 +0200

    updated warning

--- app/src/scenes/Combos/components/Info.component.js
+++ app/src/scenes/Combos/components/Info.component.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Info = () => {
     return (
-        <div>
-            <p>This is an app version of our combo chart you can find <a href="https://wiki.tripsit.me/wiki/Drug_combinations" target="_blank"> here</a> </p>
-            <p> The app, like the  combo chart, is meant as a quick reference guide and additional research <strong>MUST</strong> always be done. If you use this chart or information on your site you must link to the full summaries and display this message. </p>
+        <div style={{marginTop: 40}}>
+            <p>The app, like the  combo chart, is meant as a quick reference guide and additional research <strong>MUST</strong> always be done. For additional information check out our <a href="http://drugs.tripsit.me/" target="_blank" rel="noopener noreferrer">Factsheet.</a></p>
+            <p>Use of the data from this app is free-of-charge for non-commercial purposes. We only require that your application links back to either this page or <a href="https://wiki.tripsit.me/wiki/Drug_combinations" target="_blank" rel="noopener noreferrer">the drug combination page</a>. This should be accompanied with a note citing TripSit as the source for the particular piece(s) of information on every page which uses/displays the information. In addition that this is only intended for a quick overview we adding a note that the user themselves needs to do more research on the combination is a must.</p>
+            <a href="http://tripsit.me/contact-us/" target="_blank" rel="noopener noreferrer">Notice something wrong with this page? Let us know!</a>
         </div>
     );
 };
